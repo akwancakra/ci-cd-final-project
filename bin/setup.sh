308a2b8abbe6641a7f0d4c4de33ca991bc139c2e
akwancakra <akwancakra20@gmail.com>
2025-01-08 18:47:02 +0700

    Refactor CI workflow and update setup script for Python environment

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,36 +3,35 @@ echo "**************************************************"
 echo " Setting up CI/CD Final Project Environment"
 echo "**************************************************"
 
-# echo "*** Checking if Anaconda is installed..."
-# if ! command -v conda &> /dev/null; then
-#   echo "Anaconda could not be found, please install it first"
-#   exit 1
-# else
-#   echo "Anaconda is installed"
-# fi
+echo "*** Installing Python 3.9 and Virtual Environment"
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install -y python3.9 python3.9-venv python3.9-distutils
 
-# echo "*** Creating a Python 3.9 virtual environment with Anaconda"
-# conda create --name python-3-9-21 python=3.9 -y
-
-# echo "*** Activating the virtual environment..."
-# source activate python-3-9-21
+echo "*** Making Python 3.9 the default..."
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
 
 echo "*** Checking the Python version..."
-python --version
+python3 --version
+
+echo "*** Creating a Python virtual environment"
+python3 -m venv ~/venv
 
 echo "*** Configuring the developer environment..."
 echo "# CI/CD Final Project additions" >> ~/.bashrc
 echo "export GITHUB_ACCOUNT=$GITHUB_ACCOUNT" >> ~/.bashrc
 echo 'export PS1="\[\e]0;\u:\W\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "' >> ~/.bashrc
-echo "source activate python-3-9-21" >> ~/.bashrc
+echo "source ~/venv/bin/activate" >> ~/.bashrc
 
 echo "*** Installing Selenium and Chrome for BDD"
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sqlite3 ca-certificates chromium-driver python3-selenium
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sqlite3 ca-certificates chromium-browser chromium-chromedriver python3-selenium
 
 echo "*** Installing Python dependencies..."
-pip install --upgrade pip wheel
-pip install -r requirements.txt
+source ~/venv/bin/activate && python3 -m pip install --upgrade pip wheel
+source ~/venv/bin/activate && pip install -r requirements.txt
 
 echo "**************************************************"
 echo " CI/CD Final Project Environment Setup Complete"
